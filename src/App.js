7178f58f288629541bb5d8fd7acfd4077f90f95a
Dani <siklodidani18@gmail.com>
2021-10-08 10:44:28 +0100

    Added styling for the pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
-import api from "./api/pokemon";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+	const [allPokemon, setAllPokemon] = useState([]);
+	const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon");
 
-  const [allPokemon, setAllPokemon] = useState([]);
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon');
+	const fetchPokemon = async () => {
+		const res = await fetch(loadMore);
+		const data = await res.json();
 
-  const fetchPokemon = async () => {
-    const res = await fetch(loadMore);
-    const data = await res.json();
+		setLoadMore(data.next);
 
-    setLoadMore(data.next);
+		function createPokemonCard(result) {
+			result.forEach(async (pokemon) => {
+				const res = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+				);
+				const data = await res.json();
 
-    function createPokemonCard (result) {
-      result.forEach(async (pokemon) => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-        const data = await res.json();
+				setAllPokemon((currentPokemon) => [...currentPokemon, data]);
+			});
+		}
+		createPokemonCard(data.results);
+	};
 
-        setAllPokemon(currentPokemon => [...currentPokemon, data]);
-      })
-    }
-    createPokemonCard(data.results);
-    
-  };
-
-  useEffect(() => {
+	useEffect(() => {
 		fetchPokemon();
 	}, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>PokeTools</h1>
-          <div>
-            {allPokemon.map((pokemon, index) => 
-            <PokemonCard
-            id={pokemon.id}
-            name={pokemon.name}
-            image={pokemon.sprites.front_default}
-            key={index}
-             />)}
-          </div>
-          <button onClick={() => fetchPokemon()} >Load More</button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>PokeTools</h1>
+				<div className="pokemon-container">
+					{allPokemon.map((pokemon, index) => (
+						<PokemonCard
+							id={pokemon.id}
+							name={pokemon.name}
+							image={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+              type={pokemon.type}
+							key={index}
+						/>
+					))}
+				</div>
+				<button onClick={() => fetchPokemon()}>Load More</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,18 +1,18 @@
 import React from "react";
+import "./PokemonCard.css";
 
-const PokemonCard = ({id, name, image, type}) => {
-    return (
-        <div>
-            <div>
-            {id}
-            </div>
-            <div>
-              <img src={image} alt={name} /> 
-              {name}
-            </div>
-
-        </div>
-    )
-}
+const PokemonCard = ({ id, name, image, type }) => {
+	return (
+		<div className="pokemon">
+			<div className="name">{name}</div>
+			<div className="img-container">
+				<img src={image} alt={name} />
+			</div>
+			<div className="id">
+				<span>{id}</span>
+			</div>
+		</div>
+	);
+};
 
 export default PokemonCard;
